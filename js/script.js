661081cb6b54b03657b132dbdbc2bc44a0324cf8
Mouhamet99 <dieyemouhamet1@gmail.com>
2021-12-12 07:12:43 +0000

    Handling task removing function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = (e) => {
    let taskTitle = document.getElementById("task_title")
    let taskDescrption = document.getElementById("task_description")
    let taskDeadline = document.getElementById("task_deadline")
-   let taskPriority ;
+   let taskPriority;
 
    let addButton = document.querySelector('#btn_add')
 
@@ -11,48 +11,49 @@ window.onload = (e) => {
    /*Handling inputs */
    /********************************/
    const tasksCollection = [];
-   const task = { titre: "", description: "", priorite:"", dateLimite: ""};
+   const task = { titre: "", description: "", priorite: "", dateLimite: "", etat: "" };
 
    function HandlingInputs(e) {
       if (taskTitle.value == "" || taskDescrption.value == "" || taskDeadline.value == "") {
          taskTitle.style.borderColor = taskTitle.value != "" ? "initial" : "red";
-         taskDescrption.style.borderColor =  taskDescrption.value != "" ? "initial" : "red";
+         taskDescrption.style.borderColor = taskDescrption.value != "" ? "initial" : "red";
          taskDeadline.style.borderColor = taskDeadline.value != "" ? "initial" : "red";
          let error = document.querySelector('#error')
          error.classList.toggle('d-none')
+
          e.preventDefault()
-         // if (taskTitle.value === "") {
-         //    email.style.borderColor = "red"
-         // }
-         // if (taskDescrption.value == "") {
-         //    password.style.borderColor = "red"
-         // }
-         // if (taskPriority.value == "") {
-         //    taskPriority.style.borderColor = "red"
-         // }
       } else {
          taskPriority = document.querySelector('[name="task_priority"]:checked')
          task.titre = taskTitle.value
          task.description = taskDescrption.value
          task.priorite = taskPriority.value
-         task.dateLimite = new Date (taskDeadline.value).toUTCString()
+         task.dateLimite = new Date(taskDeadline.value).toUTCString()
          console.table(JSON.stringify(task))
          document.querySelector('#close_add_task_modal').click()
-         // let modal = document.querySelector('button.close')
-         // document.querySelector('#finish_task').addEventListener('click', function() {
-         //    $("#addTaskModal").modal("show");
-         // })
-         // window.localStorage
-         // sessionStorage.setItem("user", JSON.stringify(user))
-         
-         
       }
    }
-   
 
    addButton.addEventListener("click", function (e) {
       HandlingInputs(e)
    })
+
+   let finishTaskButtons = document.querySelectorAll('#remove_task')
+   finishTaskButtons = Array.from(finishTaskButtons)
+   let taskCards = document.querySelectorAll('.card')
+
+   finishTaskButtons.forEach((event, i)=>{
+      event.addEventListener('click', function () {
+         removeTask(finishTaskButtons, event, i);
+      })
+   })
+   /********************************/
+   /*Removing Task And Task Card */
+   /********************************/
+   function removeTask(collection, elt, index) {
+      collection.splice(index, 1);
+      elt.parentElement.parentElement.parentElement.remove();
+   }
+
 
 
 }
