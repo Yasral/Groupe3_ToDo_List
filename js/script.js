6eb7ac5fff8ffcd3f4ad96183d141ac78508eb30
Mouhamet99 <dieyemouhamet1@gmail.com>
2021-12-12 08:16:32 +0000

    Handling task showing detail function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 window.onload = (e) => {
    let formModal = document.querySelector(".form-modal")
-   let taskTitle = document.getElementById("task_title")
-   let taskDescrption = document.getElementById("task_description")
-   let taskDeadline = document.getElementById("task_deadline")
-   let taskPriority;
+   let detailsModal = document.querySelector(".showDetailsModal")
+   let taskTitleInput = document.getElementById("task_title")
+   let taskDescrptionInput = document.getElementById("task_description")
+   let taskDeadlineInput = document.getElementById("task_deadline")
+   let taskPriorityInput;
 
    let addButton = document.querySelector('#btn_add')
 
@@ -14,20 +15,20 @@ window.onload = (e) => {
    const task = { titre: "", description: "", priorite: "", dateLimite: "", etat: "" };
 
    function HandlingInputs(e) {
-      if (taskTitle.value == "" || taskDescrption.value == "" || taskDeadline.value == "") {
-         taskTitle.style.borderColor = taskTitle.value != "" ? "initial" : "red";
-         taskDescrption.style.borderColor = taskDescrption.value != "" ? "initial" : "red";
-         taskDeadline.style.borderColor = taskDeadline.value != "" ? "initial" : "red";
+      if (taskTitleInput.value == "" || taskDescrptionInput.value == "" || taskDeadlineInput.value == "") {
+         taskTitleInput.style.borderColor = taskTitleInput.value != "" ? "initial" : "red";
+         taskDescrptionInput.style.borderColor = taskDescrptionInput.value != "" ? "initial" : "red";
+         taskDeadlineInput.style.borderColor = taskDeadlineInput.value != "" ? "initial" : "red";
          let error = document.querySelector('#error')
          error.classList.toggle('d-none')
 
          e.preventDefault()
       } else {
-         taskPriority = document.querySelector('[name="task_priority"]:checked')
-         task.titre = taskTitle.value
-         task.description = taskDescrption.value
-         task.priorite = taskPriority.value
-         task.dateLimite = new Date(taskDeadline.value).toUTCString()
+         taskPriorityInput = document.querySelector('[name="task_priority"]:checked')
+         task.titre = taskTitleInput.value
+         task.description = taskDescrptionInput.value
+         task.priorite = taskPriorityInput.value
+         task.dateLimite = new Date(taskDeadlineInput.value).toUTCString()
          console.table(JSON.stringify(task))
          document.querySelector('#close_add_task_modal').click()
       }
@@ -41,7 +42,7 @@ window.onload = (e) => {
    finishTaskButtons = Array.from(finishTaskButtons)
    let taskCards = document.querySelectorAll('.card')
 
-   finishTaskButtons.forEach((event, i)=>{
+   finishTaskButtons.forEach((event, i) => {
       event.addEventListener('click', function () {
          removeTask(finishTaskButtons, event, i);
       })
@@ -54,6 +55,28 @@ window.onload = (e) => {
       elt.parentElement.parentElement.parentElement.remove();
    }
 
+   /********************************/
+   /*Show  Task details And Task Card */
+   /********************************/
+   let showDetailsTextTrigger = document.querySelectorAll('.card-body')
+
+   showDetailsTextTrigger.forEach((event, i) => {
+      event.addEventListener('click', function () {
+         handlingDetailsModalContentTask(i)
+         document.querySelector('#details_modal_trigger').click()
+      })
+   })
+   function handlingDetailsModalContentTask(index) {
+      const currentCard = document.querySelectorAll('.card')[index];
+      let taskTitle = currentCard.querySelector('#priority')
+      document.querySelector('#details_task_title').innerHTML = taskTitle.textContent
+      let taskDescrption = currentCard.querySelector('#description')
+      document.querySelector('#details_task_description').innerHTML = taskDescrption.textContent
+      let taskState = currentCard.querySelector('#state')
+      document.querySelector('#details_task_state').innerHTML = taskState.textContent
+      let taskDeadline = currentCard.querySelector('#deadline')
+      document.querySelector('#details_task_deadline').innerHTML = taskDeadline.textContent
+   }
 
 
 }
